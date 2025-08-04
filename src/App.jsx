ad56e05d16bb7bfd48fff3eedd297df21769733b
Methminpulsara <methminpulsara10@gmail.com>
2025-08-04 22:31:26 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import Navbar from "./components/Navbar"
-import Hero from  './components/Hero'
-import Features from "./components/Features "
+
 import './index.css'
-import CallToAction from "./components/CallToAction";
-import Footer from "./components/Footer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Singup";
+import Home from './pages/Home';
 
 function App() {
 
@@ -14,23 +11,11 @@ function App() {
     
     <BrowserRouter>
     <Routes>
-      {/* Landing page */}
-      <Route
-      path="/"
-      element ={
-        <div className="min-h-screen">
-          <Navbar/>
-          <Hero/>
-          <Features/>
-          <CallToAction/>
-          <Footer/>
-        </div>
-      }
-      />
-      {/* Loging navigate  */}
+      
+      <Route path="/" element ={<Home/>} />
       <Route path="/login" element ={<Login/>} />
       <Route path="/singup" element ={<Signup/>} />
-
+      
 
     </Routes>
     </BrowserRouter>
---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Hero from    '../components/Hero'
+import Features from '../components/Features '
+import CallToAction from '../components/CallToAction'
+import Footer from '../components/Footer'
+
+function Home() {
+  return (
+       
+        <div className='min-h-screen'>
+          <Navbar/>
+          <Hero/>
+          <Features/>
+          <CallToAction/>
+          <Footer/>
+        </div>
+  )
+}
+
+export default Home
