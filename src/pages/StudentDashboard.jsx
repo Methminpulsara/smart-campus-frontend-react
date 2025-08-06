9f2e0f87c79b4d061a0c5a136b634467ee0a911f
Methminpulsara <methminpulsara10@gmail.com>
2025-08-06 23:50:42 +0530

    update

--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import StudentSidebar from '../components/student/StudentSidebar'
+import StudentHeader from '../components/student/StudentHeader'
+import StudentOverview from '../components/student/StudentOverview'
+import StudentCourses from '../components/student/StudentCourses'
+import StudentAttendance from '../components/student/StudentAttendance'
 
 function StudentDashboard() {
   return (
-    <div>
-      
+    <div className='flex h-screen bg-gray-50'>
+      <StudentSidebar/>
+
+        <div className='flex flex-col flex-1'>
+            <StudentHeader/>
+                <main className='p-6 space-y-6 overflow-auto'>
+                    <StudentOverview/>
+                    <StudentCourses/>
+                    <StudentAttendance/>
+                </main>
+        </div>    
     </div>
   )
 }
