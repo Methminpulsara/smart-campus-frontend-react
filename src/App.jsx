4fbbdd8d9e2dd48030d52a86f4df38801fc9bfc2
Methminpulsara <methminpulsara10@gmail.com>
2025-08-06 23:58:36 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Singup";
 import Home from './pages/Home';
+import StudentDashboard from './pages/StudentDashboard';
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
       <Route path="/" element ={<Home/>} />
       <Route path="/login" element ={<Login/>} />
       <Route path="/singup" element ={<Signup/>} />
+      <Route path='/student/dashboard' element ={<StudentDashboard/>}/>
       
 
     </Routes>
